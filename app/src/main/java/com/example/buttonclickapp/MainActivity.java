2da974dacd86b5cecafc995162d4d76c0262aefe
TressPool <trestenpool@gmail.com>
2020-12-20 23:55:43 -0600

    Deleted old comments, OnClick method now accepts reads user input and appends to the text view

--- app/src/main/java/com/example/buttonclickapp/MainActivity.java
+++ app/src/main/java/com/example/buttonclickapp/MainActivity.java
@@ -7,13 +7,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 /** Widgets **/
+import android.text.Editable;
 import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-/** Goes back to API 9 **/
 public class MainActivity extends AppCompatActivity {
 
     /** Local Variables **/
@@ -21,54 +21,38 @@ public class MainActivity extends AppCompatActivity {
     private Button button;
     private TextView textView;
 
-    private  int numTimesClicked = 0;
-
-    /** Bundle - java class, used for passing data around **/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState); // uses the parent function onCreate()
         setContentView(R.layout.activity_main); // uses the .xml file
 
-        /** Connect Widgets **/
         // connect userInput to the widget
-        userInput = (EditText) findViewById(R.id.inputText);
-        // connect the button to the widget
-        button = (Button) findViewById(R.id.button);
+        userInput = findViewById(R.id.inputText);
+
         // connect the text view
-        textView = (TextView) findViewById(R.id.textView);
-        textView.setText("");
+        textView = findViewById(R.id.textView);
         textView.setMovementMethod(new ScrollingMovementMethod());
 
-        // create our On Click Listener
-//        View.OnClickListener onClickListener = new View.OnClickListener() {
-//
-//            @Override
-//            public void onClick(View v) {
-//                numTimesClicked ++;
-//                String result = "\nThe button got tapped " +numTimesClicked +" times";
-//                String result2 = "\nThe button got tapped 1 time";
-//                // The first click
-//                if(numTimesClicked == 1) {
-//                   textView.append(result2);
-//                }
-//                // all other clicks after
-//                else {
-//                    textView.append(result);
-//                }
-//            }
-//
-//        };
+        // connect the button to the widget
+        button = findViewById(R.id.button);
 
-        // Create the listener we just made above to our button
-//        button.setOnClickListener(onClickListener);
-
-        /** Alternative way of creating on click listener **/
+        // set the onClick method for button
         button.setOnClickListener(new View.OnClickListener(){
+            // set what we want to do when the button is clicked
             public void onClick(View v){
-                numTimesClicked ++;
-                String result = "The button got tapped " +numTimesClicked +" time";
-                textView.append(result.concat( (numTimesClicked==1)?"\n":"s\n") );
+                String result = userInput.getText().toString(); // get userInput
+                if(result.isEmpty())
+                    return;
+
+                result += "\n";
+
+                // append to text view
+                textView.append(result);
+
+                // clear the userInput field
+                userInput.setText("");
             }
+
         });
 
     }
